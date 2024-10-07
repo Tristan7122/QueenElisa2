dd1a1124a3aebcc81c40020573575cb889689560
Mateo-Chatbot <mateochatbot@gmail.com>
2024-10-07 18:10:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ module.exports = {
 ☫ Work Mode : @mode
 ☫ Work Type : @worktype`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://mateochatbot:xdtL2bYQ9eV3CeXM@gerald.r2hjy.mongodb.net/', 
   PREFIX: '/',
   LANGUAGE: 'EN',
   PORT: 3000,
